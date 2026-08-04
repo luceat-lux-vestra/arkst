a2e5c7d70ec5b7aab6ed64163d2871304123256d
luceat-lux-vestra <heathkimdev@gmail.com>
2026-08-04 23:59:29 +0900

    chore: remove spike build artifacts from tracking and update .gitignore

--- tools/spikes/typst-subprocess/target/release/deps/subprocess-1787195d421395a9.d
+++
@@ -1,5 +0,0 @@
-/Users/algorist/Repositories/scribium/tools/spikes/typst-subprocess/target/release/deps/subprocess-1787195d421395a9.d: src/main.rs
-
-/Users/algorist/Repositories/scribium/tools/spikes/typst-subprocess/target/release/deps/subprocess-1787195d421395a9: src/main.rs
-
-src/main.rs:
--- tools/spikes/typst-subprocess/target/release/subprocess.d
+++
@@ -1,1 +0,0 @@
-/Users/algorist/Repositories/scribium/tools/spikes/typst-subprocess/target/release/subprocess: /Users/algorist/Repositories/scribium/tools/spikes/typst-subprocess/src/main.rs
