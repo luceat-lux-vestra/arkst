76197c40f898f264a938aa3d8e2998c8ff27fd91
luceat-lux-vestra <heathkimdev@gmail.com>
2026-09-13 19:45:05 +0900

    chore(dev): align code patch anchors with current main

--- tools/fix_apply_v260_code.py
+++ tools/fix_apply_v260_code.py
@@ -3,10 +3,15 @@ from pathlib import Path
 p = Path("tools/apply_v260_code.py")
 text = p.read_text(encoding="utf-8")
 
-old_span = '''            source: source.clone(),\n            presentation: None,\n            span: *span,\n        }),'''
-new_span = '''            source: source.clone(),\n            presentation: None,\n            span: byte_to_source_span(span, source_id),\n        }),'''
-assert text.count(old_span) == 1, text.count(old_span)
-text = text.replace(old_span, new_span, 1)
+old_anchor = '''        } => Some(IrNode::CodeBlock {\n            language: language.clone(),\n            info: info.clone(),\n            source: source.clone(),\n            span: *span,\n        }),'''
+new_anchor = '''        } => Some(IrNode::CodeBlock {\n            language: language.clone(),\n            info: info.clone(),\n            source: source.clone(),\n            span: byte_to_source_span(span, source_id),\n        }),'''
+assert text.count(old_anchor) == 1, text.count(old_anchor)
+text = text.replace(old_anchor, new_anchor, 1)
+
+old_replacement = '''        } => Some(IrNode::CodeBlock {\n            language: language.clone(),\n            info: info.clone(),\n            source: source.clone(),\n            presentation: None,\n            span: *span,\n        }),'''
+new_replacement = '''        } => Some(IrNode::CodeBlock {\n            language: language.clone(),\n            info: info.clone(),\n            source: source.clone(),\n            presentation: None,\n            span: byte_to_source_span(span, source_id),\n        }),'''
+assert text.count(old_replacement) == 1, text.count(old_replacement)
+text = text.replace(old_replacement, new_replacement, 1)
 
 old_name = '.map_or("code", |parameter| parameter.name)'
 new_name = '.map_or("code", |parameter| parameter.name.as_str())'
