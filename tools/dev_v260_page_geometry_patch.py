1fcef33dd56547e18ac8aba85473d07d28456416
luceat-lux-vestra <heathkimdev@gmail.com>
2026-09-16 16:33:35 +0900

    ci(dev): isolate pageformat rollback regression

--- tools/dev_v260_page_geometry_patch.py
+++ tools/dev_v260_page_geometry_patch.py
@@ -1,12 +1,33 @@
 import subprocess
 
 commands = [
-    ["cargo", "test", "-p", "arkst-core", "--test", "quarkdown_v260_page_geometry", "--locked"],
-    ["cargo", "test", "-p", "arkst-core", "--test", "quarkdown_v260_page_alignment", "--locked"],
-    ["cargo", "test", "-p", "arkst-typst-inprocess", "--test", "slides_pdf_contract", "--locked"],
-    ["cargo", "test", "-p", "arkst-typst-inprocess", "--test", "auto_page_break", "--locked"],
+    [
+        "cargo",
+        "test",
+        "-p",
+        "arkst-core",
+        "--test",
+        "quarkdown_v260_page_alignment",
+        "failed_outer_alignment_conversion_rolls_back_nested_document_state_writes",
+        "--locked",
+        "--",
+        "--nocapture",
+    ],
+    [
+        "cargo",
+        "test",
+        "-p",
+        "arkst-core",
+        "--test",
+        "quarkdown_v260_page_geometry",
+        "failed_geometry_conversion_rolls_back_nested_document_state_writes",
+        "--locked",
+        "--",
+        "--nocapture",
+    ],
 ]
 
 for command in commands:
     print("+", " ".join(command), flush=True)
-    subprocess.run(command, check=True)
+    completed = subprocess.run(command)
+    print("exit:", completed.returncode, flush=True)
