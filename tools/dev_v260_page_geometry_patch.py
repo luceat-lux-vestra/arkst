fba3dc4f8c018f8dee17980f3d3d32f2269cbe64
luceat-lux-vestra <heathkimdev@gmail.com>
2026-09-16 16:40:50 +0900

    ci(dev): run slides configuration contract tests

--- tools/dev_v260_page_geometry_patch.py
+++ tools/dev_v260_page_geometry_patch.py
@@ -1,10 +1,7 @@
 import subprocess
 
 commands = [
-    ["cargo", "test", "-p", "arkst-core", "--test", "quarkdown_v260_page_geometry", "--locked"],
-    ["cargo", "test", "-p", "arkst-core", "--test", "quarkdown_v260_page_alignment", "--locked"],
-    ["cargo", "test", "-p", "arkst-typst-inprocess", "--test", "slides_pdf_contract", "--locked"],
-    ["cargo", "test", "-p", "arkst-typst-inprocess", "--test", "auto_page_break", "--locked"],
+    ["cargo", "test", "-p", "arkst-core", "--test", "quarkdown_v260_slides", "--locked"],
 ]
 
 for command in commands:
