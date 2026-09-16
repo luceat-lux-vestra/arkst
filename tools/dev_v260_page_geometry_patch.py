b88a9e7cb177cf38b6a115a2d9dca4c185c13d9c
luceat-lux-vestra <heathkimdev@gmail.com>
2026-09-17 03:24:12 +0900

    chore(dev): run slides PDF contract evidence

--- tools/dev_v260_page_geometry_patch.py
+++ tools/dev_v260_page_geometry_patch.py
@@ -1,7 +1,11 @@
 import subprocess
 
 commands = [
+    ["cargo", "test", "-p", "arkst-markdown", "--test", "quarkdown_explicit_pagebreak", "--locked"],
+    ["cargo", "test", "-p", "arkst-core", "--test", "quarkdown_explicit_pagebreak", "--locked"],
     ["cargo", "test", "-p", "arkst-core", "--test", "quarkdown_v260_slides", "--locked"],
+    ["cargo", "test", "-p", "arkst-typst", "--test", "slides_document_prelude", "--locked"],
+    ["cargo", "test", "-p", "arkst-typst-inprocess", "--test", "slides_pdf_contract", "--locked"],
 ]
 
 for command in commands:
